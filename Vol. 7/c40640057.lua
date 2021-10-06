a5b02dcd16e69b9b8325105d73e0dfbe497b0666
earthcrafterman <banddstudios@gmail.com>
2021-10-06 01:53:30 -0400

    Fixed Kuriboh's effect

--- Vol. 7/c40640057.lua
+++ Vol. 7/c40640057.lua
@@ -35,11 +35,5 @@ function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
-	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
-	Duel.RegisterEffect(e1,tp)
+	Duel.NegateAttack()
 end
